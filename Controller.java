7e3d8e65e470c6b198b9a6fec515dd055701f098
Melonial <danial@DF.BOX>
2024-08-13 22:30:42 +0800

    Added implementation for abstract button and module linkage

--- Controller.java
+++ Controller.java
@@ -1,29 +1,34 @@
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
 
 public class Controller implements ActionListener
 {
 	private BaseWindow w = new BaseWindow();
 	private Module1 counter = new Module1();
+	private Map<JButton, Controllee> dict = new HashMap<>();
 	
 	public Controller()
 	{				
 		JButton b1 = new JButton("Click!");
+		Module1 m1 = new Module1();
+		link(b1,m1);
 		b1.addActionListener(this);
 		
 		w.addComponent(b1);
+		w.addComponent(m1);
 		w.refresh();
 	}
 	
 	//Links together a button with a chosen module (must extend Controllee class)
 	public void link(JButton b, Controllee c)
 	{
-		
+		dict.put(b,c);
 	}
 	
 	public void actionPerformed(ActionEvent e)
 	{
-		System.out.println(counter.getClass().getName());
-		counter.getClass().getName().onButtonClick();
+		if(dict.get(e.getSource()) == null) {System.out.println("ERROR: Button not assigned"); System.exit(0);};	//Leave me alone, I wrote this pretty late >.>
+		dict.get(e.getSource()).onButtonClick();
 	}
 }
